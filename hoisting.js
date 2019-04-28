15aa94fc7b2c5a06a101e180849d92f8590104ac
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-28 21:22:04 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -16,14 +16,16 @@ function crazy() {
   thisIsCrazy();
 }
 
+var name = "Kristin";
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+  
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
+    
   }
+  sayMy();
 }
